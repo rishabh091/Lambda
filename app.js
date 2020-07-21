3dcd96ee8d14c7d5f971aa0231b7f5081dbdc7f8
rishabh091 <rishabhmalhotra091@gmail.com>
2020-07-21 12:18:31 +0530

    connected to mongodb atlas

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const app = express()
+const dotenv = require('dotenv')
+dotenv.config()
+const mongoose = require('mongoose')
+
+//connect to mongodb atlas
+mongoose.connect('mongodb+srv://Rishabh:' + process.env.PASSWORD + '@cluster0.4bbqk.mongodb.net/Lambda?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then((result) => {
+    console.log('Lambda connected to MongoDB Atlas.')
+})
+.catch((err) => {
+    console.log(err)
+})
+
+//starting server
+const server = app.listen(process.env.PORT || 8080, () => {
+    console.log('Lambda server started at port : ' + server.address().port)
+    console.log('Address : ' + server.address().address)
+})
+
+//test api
+app.get('/test', (req, res) => {
+    console.log('Test Fired')
+    res.send('Hello World')
+})
